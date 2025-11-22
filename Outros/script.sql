592ebadd5915cafc35b1f597c3e87d252cf57abb
HeloisyOM <heloisy.mota@sptech.school>
2025-11-21 22:50:45 -0300

    Adicionando o script atualizado

--- Outros/script.sql
+++ Outros/script.sql
@@ -4,13 +4,13 @@ use projetoIndividual;
 create table personagem(
 idPersonagem int primary key auto_increment ,
 nome varchar(45),
-imagem varchar(200)
+caminhoImg varchar(200)
 ) auto_increment=10;
 
 create table lugar(
 idLugar int primary key auto_increment,
 nome varchar(45),
-imagem varchar(200)
+caminhoImg varchar(200)
 ) auto_increment=20;
 
 create table usuario(
@@ -23,9 +23,9 @@ constraint fkLugarUsuario foreign key(fkLugar)
 	references lugar(idLugar),
 nome varchar(100) not null,
 dataNasc date not null,
-genero char(1) not null,
+genero varchar(9) not null,
 constraint chkGenero
-	check(genero in('F','M','O')),
+	check(genero in('Feminino','Masculino','Outros')),
 email varchar(255) not null unique,
 senha char(8)
 );
@@ -38,67 +38,72 @@ constraint fkMsgUser foreign key (fkUsuario)
 	references usuario(idUsuario)
 );
 
-create table quiz(
-idQuiz int,
+create table compra(
+idCompra int primary key auto_increment,
+totalGasto decimal(10,2),
+rosquinha int,
+duff int,
+panqueca int,
+pizzaLuigi int,
+buzzCola int,
+taco int,
+hamburguer int,
+hotdog int,
 fkUsuario int,
-primary key(idQuiz,fkUsuario),
-constraint fkQuizUsuario foreign key(fkUsuario)
-	references usuario(idUsuario),
-pontuacao int
+constraint fkComUser foreign key (fkUsuario) 
+	references usuario(idUsuario)
 );
 
+show tables;
 
 insert into personagem values
-(default,'Homer Simpson','.assets/imgs/img-select/img-homer'),
-(default,'Marge Simpson','.assets/imgs/img-select/img-marge'),
-(default,'Bart Simpson','.assets/imgs/img-select/img-bart'),
-(default,'Lisa Simpson','.assets/imgs/img-select/img-lisa'),
-(default,'Maggie Simpson','.assets/imgs/img-select/img-maggie'),
-(default,'Sideshow Bob','.assets/imgs/img-select/img-side'),
-(default,'Ned Flanders','.assets/imgs/img-select/img-ned'),
-(default,'Milhouse Van Houten','.assets/imgs/img-select/img-mil'),
-(default,'Krusty','.assets/imgs/img-select/img-krust'),
-(default,'Moe Szyslak','.assets/imgs/img-select/img-moe'),
-(default,'Chef Wiggum','.assets/imgs/img-select/img-wig'),
-(default,'Ralph Wiggum','.assets/imgs/img-select/img-ralph'),
-(default,'Apu Nahasapeemapetilon','.assets/imgs/img-select/img-apu'),
-(default,'Montgomery Burns','.assets/imgs/img-select/img-burns'),
-(default,'Smithers','.assets/imgs/img-select/img-smithers'),
-(default,'Comic Book Guy','.assets/imgs/img-select/img-comic'),
-(default,'Professor Frink','.assets/imgs/img-select/img-frink'),
-(default,'Nelson Muntz','.assets/imgs/img-select/img-nelson'),
-(default,'Edna Krabappel','.assets/imgs/img-select/img-edna'),
-(default,'Skinner','.assets/imgs/img-select/img-skinner'),
-(default,'Patty Bouvier','.assets/imgs/img-select/img-patty'),
-(default,'Selma Bouvier','.assets/imgs/img-select/img-selma');
-
+(default,'Homer Simpson','.assets/imgs/personagens/img-homer'),
+(default,'Marge Simpson','.assets/imgs/personagens/img-marge'),
+(default,'Bart Simpson','.assets/imgs/personagens/img-bart'),
+(default,'Lisa Simpson','.assets/imgs/personagens/img-lisa'),
+(default,'Maggie Simpson','.assets/imgs/personagens/img-maggie'),
+(default,'Sideshow Bob','.assets/imgs/personagens/img-side'),
+(default,'Ned Flanders','.assets/imgs/personagens/img-ned'),
+(default,'Milhouse Van Houten','.assets/imgs/personagens/img-mil'),
+(default,'Krusy','.assets/imgs/personagens/img-krus'),
+(default,'Moe Szyslak','.assets/imgs/personagens/img-moe'),
+(default,'Chef Wiggum','.assets/imgs/personagens/img-wig'),
+(default,'Ralph Wiggum','.assets/imgs/personagens/img-ralph'),
+(default,'Apu Nahasapeemapetilon','.assets/imgs/personagens/img-apu'),
+(default,'Montgomery Burns','.assets/imgs/personagens/img-burns'),
+(default,'Smithers','.assets/imgs/personagens/img-smithers'),
+(default,'Cara dos Quadrinhos','.assets/imgs/personagens/img-comic'),
+(default,'Nelson Muntz','.assets/imgs/personagens/img-nelson'),
+(default,'Skinner','.assets/imgs/personagens/img-skinner'),
+(default,'Patty Bouvier','.assets/imgs/personagens/img-patty'),
+(default,'Selma Bouvier','.assets/imgs/personagens/img-selma');
 
 
 insert into lugar values
-(default,'Bar do Moe','.assets/imgs/img-select/barMoe'),
-(default,'A Casa dos Simpsons','.assets/imgs/img-select/casasSimp'),
-(default,'Usina Nuclear de Springfield','.assets/imgs/img-select/usina'),
-(default,'Escola Elementar de Springfield','.assets/imgs/img-select/escola'),
-(default,'Kwik-E-Mart','.assets/imgs/img-select/kwik'),
-(default,'Igreja de Springfield','.assets/imgs/img-select/igreja'),
-(default,'Krusty Burger','.assets/imgs/img-select/img-krustyBurger'),
-(default,'Springfield Mall','.assets/imgs/img-select/img-mall'),
-(default,'Hospital Geral de Springfield','.assets/imgs/img-select/img-hospital'),
-(default,'Prefeitura de Springfield','.assets/imgs/img-select/img-prefeitura'),
-(default,'Departamento de Polícia de Springfield','.assets/imgs/img-select/img-police'),
-(default,'Casa do Flanders','.assets/imgs/img-select/img-casaFlanders'),
-(default,'Casa do Milhouse','.assets/imgs/img-select/img-casaMilhouse'),
-(default,'Central de Televisão Canal 6','.assets/imgs/img-select/img-canal6'),
-(default,'Incêndio de Pneus (Tire Fire)', '.assets/imgs/img-select/img-tireFire'),
-(default,'Museu de Springfield','.assets/imgs/img-select/img-museu'),
-(default,'Springfield Stadium','.assets/imgs/img-select/img-estadio'),
-(default,'Springfield Ásilo','.assets/imgs/img-select/img-asilo');
+(default,'Bar do Moe','.assets/imgs/lugares/barMoe'),
+(default,'A Casa dos Simpsons','.assets/imgs/lugares/casasSimp'),
+(default,'Usina Nuclear de Springfield','.assets/imgs/lugares/usina'),
+(default,'Escola Elementar de Springfield','.assets/imgs/lugares/escola'),
+(default,'Kwik-E-Mart','.assets/imgs/lugares/kwik'),
+(default,'Igreja de Springfield','.assets/imgs/lugares/igreja'),
+(default,'Krusy Burger','.assets/imgs/lugares/img-krusyBurger'),
+(default,'Springfield Mall','.assets/imgs/lugares/img-mall'),
+(default,'Hospital Geral de Springfield','.assets/imgs/lugares/img-hospital'),
+(default,'Prefeitura de Springfield','.assets/imgs/lugares/img-prefeitura'),
+(default,'Departamento de Polícia de Springfield','.assets/imgs/lugares/img-police'),
+(default,'Casa do Flanders','.assets/imgs/lugares/img-casaFlanders'),
+(default,'Central de Televisão Canal 6','.assets/imgs/lugares/img-canal6'),
+(default,'Incêndio de Pneus (Tire Fire)', '.assets/imgs/lugares/img-tireFire'),
+(default,'Museu de Springfield','.assets/imgs/lugares//img-museu'),
+(default,'Esádio de Springfield ','.assets/imgs/lugares/img-esadio'),
+(default,'Springfield Ásilo','.assets/imgs/lugares/img-asilo');
 
 
 select * from personagem;
 select * from lugar;
 select * from usuario;
 select * from mensagem;
+select * from compra;
 show tables;
 
 /*Gráfico do top 3 personagens favoritos*/
@@ -110,7 +115,7 @@ group by p.nome
 order by total desc
 limit 3;
 
-/*Gráfico top 3 lugares que as pessoas mais gostariam de visitar ou apenas gostam bastante*/
+/*Gráfico top 3 lugares que as pessoas mais gosariam de visitar ou apenas gosam basante*/
 select l.nome as lugar,
 count(u.fkLugar) as total
 from lugar as l join usuario as u
@@ -119,7 +124,7 @@ group by l.nome
 order by total desc
 limit 3;
 
-/*Gráfico de distribuição de gênero*/
+/*Gráfico de disribuição de gênero*/
 
 select genero, COUNT(genero) as total
 from usuario
@@ -150,3 +155,45 @@ from usuario
 group by genero
 order by count(genero) desc
 limit 1;
+
+/*KPI média de compra da Comunidade*/
+
+select ROUND(avg(totalGasto)) as MediaGeral from compra;
+
+/*Idade*/
+	select AVG(timestampdiff(year,dataNasc, curdate())) as idade from usuario;
+select timestampdiff(year,dataNasc, curdate()) as idade from usuario;
+
+/*grafico de distribuicao de faixa etária */
+select
+case 
+when timestampdiff(year,dataNasc, curdate()) between 0 and 12 then '0-12'
+when timestampdiff(year,dataNasc, curdate()) between 13 and 	17 then '13-17'
+when timestampdiff(year,dataNasc, curdate()) between 18 and 25 then '18-25'
+when timestampdiff(year,dataNasc, curdate()) between 26 and 40 then '26-40'
+else '41+'
+end as faixa,
+	count(*) as valores
+    from usuario
+    group by faixa
+		order by valores desc;
+
+select DISTINCT(totalGasto) from compra
+order by totalGasto desc
+limit 1;
+
+select SUM(totalGasto) from compra
+order by totalGasto desc;
+
+/*Gŕafico de faixa de total gasto no mercadinh*/
+select 
+case
+when totalGasto<=20 then 'Até 20 reais'
+when totalGasto>20 and totalGasto<=40 then 'Até R$40,00 reais'
+when totalGasto>40 and totalGasto<=70 then 'Até R$70,00 reais'
+when totalGasto>70 and totalGasto<=100 then 'Até R$100,00 reais'
+when totalGasto>100 and totalGasto<=150 then 'Até R$150,00 reais'
+when totalGasto>150 and totalGasto<=200 then 'Até R$200,00'
+else 'Maior que R$200,00'
+end as faixaGasto
+from compra;
